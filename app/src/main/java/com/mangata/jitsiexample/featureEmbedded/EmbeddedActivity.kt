e6257486e3a733532fa68a6aa44dda172971d1e5
Tomislav Godek <tomislav8462@gmail.com>
2022-03-29 15:51:38 +0200

    Added manual video resize on screen rotation

--- app/src/main/java/com/mangata/jitsiexample/featureEmbedded/EmbeddedActivity.kt
+++ app/src/main/java/com/mangata/jitsiexample/featureEmbedded/EmbeddedActivity.kt
@@ -1,8 +1,11 @@
-package com.mangata.jitsiexample.feature_embedded
+package com.mangata.jitsiexample.featureEmbedded
 
 import android.Manifest
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.ActivityInfo
 import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
@@ -11,12 +14,11 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.view.doOnPreDraw
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.facebook.react.modules.core.PermissionListener
 import com.mangata.jitsiexample.databinding.ActivityEmbeddedBinding
 import com.mangata.jitsiexample.util.navigationBarHeight
-import org.jitsi.meet.sdk.JitsiMeetActivityDelegate
-import org.jitsi.meet.sdk.JitsiMeetActivityInterface
-import org.jitsi.meet.sdk.JitsiMeetView
+import org.jitsi.meet.sdk.*
 import org.koin.androidx.viewmodel.ext.android.stateViewModel
 
 @RequiresApi(Build.VERSION_CODES.R)
@@ -25,6 +27,15 @@ class EmbeddedActivity : AppCompatActivity(), JitsiMeetActivityInterface {
     private val viewModel: EmbeddedViewModel by stateViewModel(state = { Bundle(intent.extras) })
     private lateinit var binding: ActivityEmbeddedBinding
     private var jitsiMeetView: JitsiMeetView? = null
+    private var portraitScreenWidth: Int? = null
+    private var portraitScreenHeight: Int? = null
+    private var portraitFrameHeight: Int? = null
+
+    private val broadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            onBroadcastReceived(intent)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,28 +43,38 @@ class EmbeddedActivity : AppCompatActivity(), JitsiMeetActivityInterface {
         binding = ActivityEmbeddedBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val videoView = binding.customFrameLayout
+        val videoView = binding.frameLayout
         jitsiMeetView = JitsiMeetView(this)
 
-        // If the calculated height of the Frame Layout is less than 900dp
-        // we need to force the layout height to at least 900dp else the video call options won't show
+        registerForBroadcastMessages()
+
+        /**
+         * If the calculated height of the Frame Layout is less than 900dp
+         * we need to force the layout height to at least 900dp else the video call options won't show
+         */
         setupVideoFrame(videoView)
 
-        if (!viewModel.conferenceJoined)
+        if (!viewModel.conferenceJoined) {
             jitsiMeetView?.join(viewModel.onConferenceJoinConfig())
+        }
     }
-
 
     private fun setupVideoFrame(videoView: FrameLayout) {
         videoView.doOnPreDraw {
             val rootWidth = resources.displayMetrics.widthPixels
             val rootHeight = resources.displayMetrics.heightPixels
-            var videoHeight = it.height
-            var videoWidth = it.width
             val orientation = resources.configuration.orientation
 
-            if (videoHeight < 900 && orientation == Configuration.ORIENTATION_PORTRAIT)
+            var videoHeight = it.height
+            var videoWidth = it.width
+
+            if (videoHeight < 900 && orientation == Configuration.ORIENTATION_PORTRAIT) {
                 videoHeight = 900
+
+                portraitScreenWidth = rootWidth
+                portraitScreenHeight = rootHeight + navigationBarHeight
+                portraitFrameHeight = videoHeight
+            }
 
             if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
                 videoWidth = rootWidth + navigationBarHeight
@@ -68,6 +89,26 @@ class EmbeddedActivity : AppCompatActivity(), JitsiMeetActivityInterface {
         }
     }
 
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        val orientation = newConfig.orientation
+
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            binding.frameLayout.layoutParams.width = portraitScreenHeight!!
+            binding.frameLayout.layoutParams.height = portraitScreenWidth!!
+
+            jitsiMeetView?.layoutParams?.width = FrameLayout.LayoutParams.MATCH_PARENT
+            jitsiMeetView?.layoutParams?.height = FrameLayout.LayoutParams.MATCH_PARENT
+        }
+
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            binding.frameLayout.layoutParams.width = portraitScreenWidth!!
+            binding.frameLayout.layoutParams?.height = portraitFrameHeight!!
+
+            jitsiMeetView?.layoutParams?.width = FrameLayout.LayoutParams.MATCH_PARENT
+            jitsiMeetView?.layoutParams?.height = FrameLayout.LayoutParams.MATCH_PARENT
+        }
+    }
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -116,5 +157,25 @@ class EmbeddedActivity : AppCompatActivity(), JitsiMeetActivityInterface {
         jitsiMeetView?.dispose()
         jitsiMeetView = null
         JitsiMeetActivityDelegate.onHostDestroy(this)
+    }
+
+    private fun registerForBroadcastMessages() {
+        val intentFilter = IntentFilter()
+
+        for (type in BroadcastEvent.Type.values()) {
+            intentFilter.addAction(type.action)
+        }
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, intentFilter)
+    }
+
+    private fun onBroadcastReceived(intent: Intent?) {
+        intent?.let {
+            val event = BroadcastEvent(it)
+            when (event.type) {
+                BroadcastEvent.Type.CONFERENCE_JOINED -> println("Conference Joined with url ${event.data["url"]}")
+                BroadcastEvent.Type.PARTICIPANT_JOINED -> println("Participant joined ${event.data["name"]}")
+                else -> return
+            }
+        }
     }
 }
